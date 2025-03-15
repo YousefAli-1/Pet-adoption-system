c87979f3084de9ad9f2606627b21f76960c176e5
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-03-15 13:08:51 +0200

    Getting Initial layout of dashboard

--- src/app/shelters/dashboard/dashboard.component.ts
+++ src/app/shelters/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TilesComponent } from "../shared/tiles/tiles.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [TilesComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })

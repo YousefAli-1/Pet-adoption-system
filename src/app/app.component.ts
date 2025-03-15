80d472cb183ade889e7981bc99b842422e66f4f3
YousefAli-1 <yousefalisaied@gmail.com>
2025-03-16 01:08:19 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
-import { UnauthorizedComponent } from "./unauthorized/unauthorized.component";
-import { LoginComponent } from "./login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NotFoundComponent, UnauthorizedComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

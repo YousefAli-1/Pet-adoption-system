5ee07cc018327c0e6e36809e19abf1f070828fd1
YousefAli-1 <yousefalisaied@gmail.com>
2025-03-16 01:04:24 +0200

    edit to the footer+login+unauthorized+404 pages no routings yet and no functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { UnauthorizedComponent } from "./unauthorized/unauthorized.component";
+import { LoginComponent } from "./login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NotFoundComponent, UnauthorizedComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

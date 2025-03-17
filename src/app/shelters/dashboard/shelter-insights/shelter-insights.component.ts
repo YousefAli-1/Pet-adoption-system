a37f8782f123014925722e93c1fa92767bc89b9c
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-03-17 18:16:39 +0200

    Shelter Insights implemented

--- src/app/shelters/dashboard/shelter-insights/shelter-insights.component.ts
+++ src/app/shelters/dashboard/shelter-insights/shelter-insights.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, computed, inject, OnInit, PLATFORM_ID, signal, Signal } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+
+type ShelterType={name: String, statusCount: {adoptedCount: number, waitingForAVisitCount: number,returnedCount: number, waitingForAdoptionCount: number}};
 
 @Component({
   selector: 'app-shelter-insights',
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './shelter-insights.component.html',
-  styleUrl: './shelter-insights.component.scss'
+  styleUrl: './shelter-insights.component.scss',
 })
-export class ShelterInsightsComponent {
+export class ShelterInsightsComponent implements OnInit {
 
+  shelter=signal<ShelterType>({
+    name: 'Zee Animal',
+    statusCount:{
+      adoptedCount: 10,
+      waitingForAVisitCount: 4,
+      returnedCount: 1,
+      waitingForAdoptionCount: 15
+    }
+  });
+  data: Signal<any>=signal({});
+
+  options: any;
+
+  platformId = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    this.InitChart();
+  }
+  InitChart() : void {
+    if (isPlatformBrowser(this.platformId)) {
+
+      this.data = computed(()=>{
+        return {
+        labels: ['Adopted','Waiting for a visit','Returned','Waiting for adoption'],
+        datasets: [
+          {
+            data: [this.shelter().statusCount.adoptedCount, this.shelter().statusCount.waitingForAVisitCount, this.shelter().statusCount.returnedCount, this.shelter().statusCount.waitingForAdoptionCount],
+            backgroundColor: [
+              ' #fc6300 ', ' #fe912a ', '  #625f5e  ', '  #303030  '
+            ],
+            hoverBackgroundColor: [
+              'rgba(252, 101, 0, 0.9) ', 'rgba(254, 144, 42, 0.9) ', 'rgba(98, 95, 94, 0.9)  ', 'rgba(48, 48, 48, 0.9)  '
+            ],
+          },
+        ],
+      }});
+      
+      this.options = {
+        plugins: {
+          legend: {
+            labels: {
+              usePointStyle: true,
+              color: '#333',
+            },
+          },
+        },
+      };
+    }
+  }
 }

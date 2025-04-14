a69e0b133a9241a09b6a329d24508eb157aea752
Yousef Ali <132370936+YousefAli-1@users.noreply.github.com>
2025-04-14 19:36:11 +0200

    mini updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,18 @@ import { LoginComponent } from "./login/login.component";
 })
 export class AppComponent {
   title = 'Pet-adoption-system';
+  constructor() {
+    // Listen for router navigation events
+    window.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.matches('a[routerLink]')) {
+        if (!target.textContent?.toLowerCase().includes('contact us')) {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }
+      }
+    });
+  }
 }

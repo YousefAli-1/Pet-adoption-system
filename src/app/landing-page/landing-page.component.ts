3cf62b853d405adcc6da2213de0e3f7e91d67212
Yousef Ali <132370936+YousefAli-1@users.noreply.github.com>
2025-04-14 17:30:57 +0200

    landing page intial commit and header updates

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  selectedType: string = 'all';
+  filteredPets: any[] = [];
+
+  constructor() {
+    this.filteredPets = this.pets;
+  }
+  pets: any[] = [
+    {
+      name: 'Max',
+      type: 'dogs',
+      breed: 'Golden Retriever',
+      age: 2,
+      imageUrl: 'Golden Retriever Puppy.jpeg'
+    },
+    {
+      name: 'Luna',
+      type: 'cats', 
+      breed: 'Persian',
+      age: 1,
+      imageUrl: 'egyptian mau.PNG'
+    },
+    {
+      name: 'Charlie',
+      type: 'birds',
+      breed: 'Parakeet',
+      age: 3,
+      imageUrl: 'Bird.jpg'
+    },
+    {
+      name: 'Oreo',
+      type: 'others',
+      breed: 'Rabbit',
+      age: 1,
+      imageUrl: 'Rabbit.jpg'
+    }
+  ];
+
+  filterPets(type: string): void {
+    this.selectedType = type;
+    if (type === 'all') {
+      this.filteredPets = this.pets;
+    } else {
+      this.filteredPets = this.pets.filter(pet => pet.type === type);
+    }
+    console.log(this.filteredPets);
+  }
 
 }

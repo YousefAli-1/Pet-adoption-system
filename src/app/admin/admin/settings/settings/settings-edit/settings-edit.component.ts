04a58b18b39bad7c021b4e35ed625b486bec1745
Mostafa Zakaria <mostafazakaria848@gmail.com>
2025-05-21 01:37:58 +0200

    final bug fixes

--- src/app/admin/admin/settings/settings/settings-edit/settings-edit.component.ts
+++ src/app/admin/admin/settings/settings/settings-edit/settings-edit.component.ts
@@ -8,16 +8,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { inject,Injectable, signal } from '@angular/core';
 import { Admin } from '../../../admin.models';
 import { AdminSettingsUpdate } from '../../../adminSettings.update';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-settings-edit',
   imports: [FormsModule, MatFormFieldModule, MatInputModule,
-     MatSlideToggleModule, MatCardModule, MatButtonModule],
+     MatSlideToggleModule, MatCardModule, MatButtonModule, RouterModule, NgIf],
   templateUrl: './settings-edit.component.html',
   styleUrl: './settings-edit.component.scss'
 })
 export class SettingsEditComponent {
-  private adminService = inject(AdminSettingsUpdate);
+  public adminService = inject(AdminSettingsUpdate);
     admin: Admin = this.adminService.getLoggedInAdminSignal();
     showPassword = false; 
     showConfirmPassword = false;
@@ -26,9 +29,15 @@ export class SettingsEditComponent {
     saveChanges() {
       if (this.confirmPassword !== this.admin.password) {
         this.adminService.triggerError('Passwords do not match! Try Again');
+        console.log('s1 -',this.confirmPassword,'s2 -', this.admin.password);
+        console.log('m1',localStorage.getItem('loggedInAdmin'));
+
         return;
       }
+
       this.adminService.updateLoggedInAdmin(this.admin);
       this.adminService.trigger('Account info saved successfully!');
+      console.log('m3',localStorage.getItem('loggedInAdmin'));
+
 }
 }
